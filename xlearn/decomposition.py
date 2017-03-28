7de36b0aae0963569409c72d22ab27bf8fc717bd
Wenchang Yang <yang.wenchang@uci.edu>
2017-03-28 16:09:38 -0700

    decomposition.py update

--- xlearn/decomposition.py
+++ xlearn/decomposition.py
@@ -12,7 +12,8 @@ class PCA(PCA_sklearn):
 
     def __init__(self, *args, **kw):
         weight = kw.pop('weight', None)
-        super().__init__(*args, **kw)
+        n_components = kw.pop('n_components', 1)
+        super().__init__(*args, n_components=n_components, **kw)
         self.weight = weight
     def __str__(self):
         attrs = ['copy', 'iterated_power', 'n_components', 'random_state', 'svd_solver',
@@ -34,6 +35,8 @@ class PCA(PCA_sklearn):
         '''xarray.DataArray version of sklearn.decomposition.PCA.fit.'''
 
         # Compatible with the slearn.decomposition.PCA fit method when the input data are not DataArray
+        da = da.copy()
+        
         if not isinstance(da, xr.DataArray):
             X = da
             return super().fit(X)
@@ -57,24 +60,33 @@ class PCA(PCA_sklearn):
         eofs = np.empty((self.n_components_, n_grids)) * np.nan
         eofs[:, valid_grids] = self.components_
         eofs = eofs.reshape((self.n_components_,) + grid_shape)
+        # reshape the mean_
+        mean_ = np.empty(n_grids) * np.nan
+        mean_[valid_grids] = self.mean_
+        mean_ = mean_.reshape(grid_shape)
         
         # wrap regression coefficient into DataArray
         # dims
         grid_dims = da.dims[1:]
         eofs_dims = ('mode',) + grid_dims
         # coords
-        eofs_coords = {eofs_dims[0]: np.arange(self.n_components_)}
-        for dim in grid_dims:
-            eofs_coords[dim] = da[dim]
+        grid_coords = {dim: da[dim] for dim in grid_dims}
+        eofs_coords = grid_coords.copy()
+        eofs_coords[eofs_dims[0]] = np.arange(self.n_components_)
         # DataArray
         self.components_da = xr.DataArray(eofs,
             dims=eofs_dims, coords=eofs_coords)
+        # self.mean_
+        self.mean_da = xr.DataArray(mean_,
+            dims=grid_dims, coords=grid_coords)
+        
         
         return self
     
     def transform(self, da):
         '''xarray version of sklearn.decomposition.PCA.transform'''
         # Compatible with the slearn.decomposition.PCA fit method when the input data are not DataArray
+        da = da.copy()
         if not isinstance(da, xr.DataArray):
             X = da
             return super().transform(X)
@@ -110,6 +122,7 @@ class PCA(PCA_sklearn):
     def inverse_transform(self, da):
         '''xarray version of sklearn.decomposition.PCA.inverse_transform'''
         # Compatible with the slearn.decomposition.PCA fit method when the input data are not DataArray
+        da = da.copy()
         if not isinstance(da, xr.DataArray):
             X = da
             return super().inverse_transform(X)
